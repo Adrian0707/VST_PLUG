6ed3fac341a9f20dd766de4116b47d5405749d5a
Adrian0707 <39618927+Adrian0707@users.noreply.github.com>
2022-01-11 01:12:51 +0100

    GUI update

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -17,25 +17,33 @@ VST_PLUGAudioProcessorEditor::VST_PLUGAudioProcessorEditor (VST_PLUGAudioProcess
     addAndMakeVisible(azimuthKnob = new Slider("Azimuth"));
     azimuthKnob->setSliderStyle(Slider::Rotary);
     azimuthKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+    azimuthKnob->addListener(this);
+    
 
     addAndMakeVisible(elevationKnob = new Slider("Elevation"));
     elevationKnob->setSliderStyle(Slider::SliderStyle::LinearVertical);
     elevationKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+    elevationKnob->addListener(this);
 
     addAndMakeVisible(rollKnob = new Slider("Roll"));
     rollKnob->setSliderStyle(Slider::SliderStyle::Rotary);
     rollKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+    rollKnob->addListener(this);
 
     addAndMakeVisible(widthKnob = new Slider("Width"));
     widthKnob->setSliderStyle(Slider::SliderStyle::LinearVertical);
     widthKnob->setTextBoxStyle(Slider::NoTextBox, false, 100, 100);
+    widthKnob->addListener(this);
 
     azimuthAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(),"azimuth", *azimuthKnob );
     elevationAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "elevation", *elevationKnob);
     rollAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "roll", *rollKnob);
     widthAttachment = new AudioProcessorValueTreeState::SliderAttachment(p.getState(), "width", *widthKnob);
-    
+    headSideImage = ImageFileFormat::loadFrom(File("D:/PW-MAGISTERSKIE/PW-Magisterskie-sem2/SPD/PLUG_VST/VST_PLUG/Source/head_side.h"));
+    //headTopImage = ImageFileFormat::loadFrom(File("head_top.h"));
     setSize (600, 500);
+
+    startTimerHz(30);
 }
 
 VST_PLUGAudioProcessorEditor::~VST_PLUGAudioProcessorEditor()
@@ -45,7 +53,8 @@ VST_PLUGAudioProcessorEditor::~VST_PLUGAudioProcessorEditor()
 //==============================================================================
 void VST_PLUGAudioProcessorEditor::paint (Graphics& g)
 {
-    //g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+    
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
     g.drawText("Azimuth", ((getWidth() / 5) * 1) - (100 / 2), (getHeight() / 5) - (100 / 2) + 50, 100, 100, Justification::centred, false);
     g.drawText(std::to_string((int)azimuthKnob->getValue()), ((getWidth() / 5) * 1) - (100 / 2), (getHeight() / 5) - (100 / 2) + 5, 100, 100, Justification::centred, false);
     g.drawText("Elevation", ((getWidth() / 5) * 2) - (100 / 2), (getHeight() / 5) - (100 / 2) + 50, 100, 100, Justification::centred, false);
@@ -55,11 +64,11 @@ void VST_PLUGAudioProcessorEditor::paint (Graphics& g)
     g.drawText("Width", ((getWidth() / 5) * 4) - (100 / 2), (getHeight() / 5) - (100 / 2) + 50, 100, 100, Justification::centred, false);
     g.drawText(std::to_string((int)widthKnob->getValue()), ((getWidth() / 5) * 4) - (100 / 2), (getHeight() / 5) - (100 / 2) - 50, 100, 100, Justification::centred, false);
 
-    g.setColour(Colour::fromRGB(256, 100, 100));
-    g.fillEllipse((getWidth() / 2) - 250, (getHeight() / 2), 200, 200);
-
-    g.setColour(Colour::fromRGB(256, 100, 100));
-    g.fillEllipse((getWidth() / 2) + 50, (getHeight() / 2), 200, 200);
+    //First circle
+    for (int i = 0; i < 10; i++) {
+        g.setColour(Colour::fromRGBA(155, 150, 0, 50));
+        g.fillEllipse((getWidth() / 2) - 250 + 10 * i, (getHeight() / 2) + 10 * i, 200- 20*i, 200-20*i);
+    }
 
     g.setColour(Colour::fromRGB(0, 0, 0));
 
@@ -81,20 +90,58 @@ void VST_PLUGAudioProcessorEditor::paint (Graphics& g)
         100,
         100,
         Justification::centred, false);
+    g.setColour(Colour::fromRGB(0, 0, 255));
     g.drawText("Left",
         (getWidth() / 2) - 320,
         (getHeight() / 2) + 50,
         100,
         100,
         Justification::centred, false);
-    g.drawText("Rigth",
+    g.setColour(Colour::fromRGB(255, 0, 0));
+    g.drawText("Right",
         (getWidth() / 2) - 70,
         (getHeight() / 2) + 50,
         100,
         100,
         Justification::centred, false);
+    //R chanel
+    g.setColour(Colour::fromRGBA(255, 0, 0, 100));
+    g.fillEllipse(
+        ((getWidth() / 2) - 155 + 100 * cos((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * cos((((int)rollKnob->getValue()) * 3.14) / 180),
+        ((getHeight() / 2) + 95 + 100 * sin((((int)azimuthKnob->getValue() - 90) * 3.14) / 180))+ (int)widthKnob->getValue()* sin((((int)rollKnob->getValue()) * 3.14) / 180),
+        10,
+        10);
+    g.setColour(Colour::fromRGB(0, 0, 0));
+    g.drawText("R",
+        ((getWidth() / 2) - 155 + 100 * cos((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * cos((((int)rollKnob->getValue()) * 3.14) / 180),
+        ((getHeight() / 2) + 95 + 100 * sin((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * sin((((int)rollKnob->getValue()) * 3.14) / 180),
+        10,
+        10,
+        Justification::centred, false);
 
-
+    //Left channel
+    g.setColour(Colour::fromRGBA(0, 0, 255, 100));
+    g.fillEllipse(
+        ((getWidth() / 2) - 155 + 100 * cos((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * cos((((int)rollKnob->getValue()-180) * 3.14) / 180),
+        ((getHeight() / 2) + 95 + 100 * sin((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * sin((((int)rollKnob->getValue()-180) * 3.14) / 180),
+        10,
+        10);
+    g.setColour(Colour::fromRGB(0, 0, 0));
+    g.drawText("L",
+        ((getWidth() / 2) - 155 + 100 * cos((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * cos((((int)rollKnob->getValue() - 180) * 3.14) / 180),
+        ((getHeight() / 2) + 95 + 100 * sin((((int)azimuthKnob->getValue() - 90) * 3.14) / 180)) + (int)widthKnob->getValue() * sin((((int)rollKnob->getValue() - 180) * 3.14) / 180),
+        10,
+        10,
+        Justification::centred, false);
+    
+    //Secound circle
+    g.setColour(Colour::fromRGB(256, 100, 100));
+    //g.fillEllipse((getWidth() / 2) + 50, (getHeight() / 2), 200, 200);
+    for (int i = 0; i < 10; i++) {
+        g.setColour(Colour::fromRGBA(256, 100, 100, 50));
+        g.fillEllipse((getWidth() / 2) + 50 + 10 * i, (getHeight() / 2) + 10 * i, 200 - 20 * i, 200 - 20 * i);
+    }
+    g.setColour(Colour::fromRGB(0, 0, 0));
     g.drawArrow(Line<float>(
         (getWidth() / 2) + 150,
         (getHeight() / 2) + 100,
@@ -116,6 +163,7 @@ void VST_PLUGAudioProcessorEditor::paint (Graphics& g)
         100,
         Justification::centred, false);
    
+    g.drawImage(headSideImage, 0, 0, 100, 100,100,100,0,0);
 
 }
 
@@ -126,4 +174,16 @@ void VST_PLUGAudioProcessorEditor::resized()
     rollKnob->setBounds(((getWidth() / 5) * 3) - (100 / 2), (getHeight() /5) - (100 / 2), 100, 100);
     widthKnob->setBounds(((getWidth() / 5) * 4) - (100 / 2), (getHeight() / 5) - (100 / 2), 100, 100);
     repaint();
+}
+
+void VST_PLUGAudioProcessorEditor::timerCallback()
+{
+    if (parametersChanged.compareAndSetBool(false,true)) {
+        repaint();
+    }
+}
+
+void VST_PLUGAudioProcessorEditor::sliderValueChanged(Slider* slider)
+{
+    parametersChanged.set(true);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -15,7 +15,9 @@ using namespace juce;
 //==============================================================================
 /**
 */
-class VST_PLUGAudioProcessorEditor  : public AudioProcessorEditor
+class VST_PLUGAudioProcessorEditor  : public AudioProcessorEditor,
+    public Timer,
+    public Slider::Listener
 {
 public:
     VST_PLUGAudioProcessorEditor (VST_PLUGAudioProcessor&);
@@ -25,7 +27,13 @@ public:
     void paint (Graphics&) override;
     void resized() override;
 
+
+
 private:
+    Atomic<bool> parametersChanged{ false };
+
+    Image headTopImage;
+    Image headSideImage;
 
     ScopedPointer<Slider> azimuthKnob;
     ScopedPointer<Slider> elevationKnob;
@@ -40,4 +48,10 @@ private:
     VST_PLUGAudioProcessor& audioProcessor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (VST_PLUGAudioProcessorEditor)
+
+    // Odziedziczono za poœrednictwem elementu Timer
+    virtual void timerCallback() override;
+
+    // Odziedziczono za poœrednictwem elementu Listener
+    virtual void sliderValueChanged(Slider* slider) override;
 };
